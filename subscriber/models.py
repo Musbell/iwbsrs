03f94ad28cca5a699646459f0dcf8f1b6940df58
Bello Musa <musbell007@gmail.com>
2017-11-09 16:17:49 +0100

    modified image field

--- subscriber/models.py
+++ subscriber/models.py
@@ -22,8 +22,8 @@ class Subscriber(AbstractUser):
     nationality = models.CharField(max_length=30)
     # width = models.IntegerField(default=300)
     # height = models.IntegerField(default=300)
-    image = models.FileField(verbose_name='Captured image',)
-    # image = models.FileField(verbose_name='Captured image')
+    # image = models.FileField(verbose_name='Captured image', width_field='width', height_field='height',)
+    image = models.ImageField(verbose_name='Captured image')
     role = models.CharField(max_length=30,choices=USER_TYPE,default='SUBSCRIBER')
 
 
